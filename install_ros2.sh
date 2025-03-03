5c6131a4439ff7445987a2ddf01a61708c63fff4
Trần Bá Hiến <69721832+tranhien1612@users.noreply.github.com>
2025-03-03 09:36:00 +0700

    Update install_ros2.sh

--- install_ros2.sh
+++ install_ros2.sh
@@ -1,7 +1,63 @@
 #!/bin/bash
 
-# Define variables
-ROS2_DISTRO="jazzy"
+detect_ubuntu_version() {
+    UBUNTU_VERSION=$(lsb_release -cs)
+    case $UBUNTU_VERSION in
+        "focal")
+            ROS2_DISTRO="foxy"
+            ;;
+        "jammy")
+            ROS2_DISTRO="humble"
+            ;;
+        *)
+            echo "Unsupported Ubuntu version: $UBUNTU_VERSION"
+            echo "Supported versions are: 20.04 (Focal) and 22.04 (Jammy)"
+            exit 1
+            ;;
+    esac
+}
+
+install_ros2() {
+   echo "Installing ROS 2 $ROS2_DISTRO for Ubuntu $UBUNTU_VERSION..."
+   # Update package list
+   echo "Updating package list..."
+   apt update -y
+   
+   echo "Set locale..."
+   apt install locales
+   locale-gen en_US en_US.UTF-8
+   update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+   export LANG=en_US.UTF-8
+   locale
+   
+   echo "Enable required respositories..."
+   apt install software-properties-common
+   add-apt-repository universe
+   
+   echo "Adding ROS 2 GPG key..."
+   apt install curl -y
+   curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+   
+   echo "Adding ROS 2 repository..."
+   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+   
+   echo "Install development tools..."
+   apt update -y && apt install ros-dev-tools
+   
+   echo "Installing ROS 2 desktop package..."
+   apt update -y
+   apt upgrade -y
+   apt install ros-$ROS2_DISTRO-desktop
+   apt install python3-colcon-common-extensions python3-argcomplete ros-dev-tools
+   
+   # Source the ROS 2 setup file
+   echo "Sourcing ROS 2 setup file..."
+   source /opt/ros/$ROS2_DISTRO/setup.bash
+}
+
+# Main script execution
+echo "Checking Ubuntu version..."
+detect_ubuntu_version
 
 # Check if the script is run with sudo privileges
 if [[ $EUID -ne 0 ]]; then
@@ -9,38 +65,5 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-# Update package list
-echo "Updating package list..."
-apt update -y
-
-echo "Set locale..."
-apt install locales
-locale-gen en_US en_US.UTF-8
-update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
-export LANG=en_US.UTF-8
-locale
-
-echo "Enable required respositories..."
-apt install software-properties-common
-add-apt-repository universe
-
-echo "Adding ROS 2 GPG key..."
-apt install curl -y
-curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-
-echo "Adding ROS 2 repository..."
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-
-echo "Install development tools..."
-apt update -y && apt install ros-dev-tools
-
-echo "Installing ROS 2 desktop package..."
-apt update -y
-apt upgrade -y
-apt install ros-$ROS2_DISTRO-desktop
-apt install python3-colcon-common-extensions python3-argcomplete ros-dev-tools
-
-# Source the ROS 2 setup file
-echo "Sourcing ROS 2 setup file..."
-source /opt/ros/$ROS2_DISTRO/setup.bash
-
+# Install ROS 2
+install_ros2
